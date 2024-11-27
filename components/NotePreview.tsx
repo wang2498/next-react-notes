d1f82de119b55665d9f0c3c5285c032cac7f1f69
wang <wang2498163@163.com>
2024-11-27 20:39:18 +0800

    fix: 修复lint问题

--- components/NotePreview.tsx
+++ components/NotePreview.tsx
@@ -4,13 +4,13 @@ import { marked } from 'marked'
 const allowedTags = sanitizeHtml.defaults.allowedTags.concat(['img', 'h1', 'h2', 'h3'])
 
 const allowedAttributes = Object.assign({}, sanitizeHtml.defaults.allowedAttributes, { img: ['alt', 'src'] })
-export default function NotePreview({ children }: { children: string }) {
+export default function NotePreview({ children }: { children: any }) {
   return (
     <div className="note-preivew">
       <div
         className="text-with-markdown"
         dangerouslySetInnerHTML={{
-          __html: sanitizeHtml(marked(children || ''), {
+          __html: sanitizeHtml(marked(children || '') as string, {
             allowedTags,
             allowedAttributes,
           }),
